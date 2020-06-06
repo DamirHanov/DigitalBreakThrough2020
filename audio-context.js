394df57d6e514706571d29eb05324164a4497052
Maxsior <maxsior@mail.ru>
2020-06-06 15:59:56 +0500

    speak animation

---
+++ audio-context.js
@@ -0,0 +1,32 @@
+function initAudioContext(stream, onVolumeChange) {
+    console.log(1);
+
+    var context = new AudioContext();
+    var source = context.createMediaStreamSource(stream);
+
+    var analyser = context.createAnalyser();
+
+    var processorNode = context.createScriptProcessor(2048, 1, 1);
+    var array = new Uint8Array(analyser.frequencyBinCount);
+
+    // Connect the audioinput to the analyser node
+    source.connect(analyser);
+    analyser.connect(processorNode);
+    processorNode.connect(context.destination);
+
+    processorNode.onaudioprocess = function () {
+        analyser.getByteFrequencyData(array);
+        var averageVolume = getAverageVolume(array)
+        requestAnimationFrame(() => onVolumeChange(averageVolume));
+    }
+
+    function getAverageVolume(array) {
+        var amplitudeSum = 0;
+
+        for (var i = 0; i < array.length; i++) {
+            amplitudeSum += array[i];
+        }
+
+        return amplitudeSum / array.length;
+    }
+}
