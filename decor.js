3a3cad1538dc880c054856c51531a9fe510cdcf7
Damir Hanov <damir.khanof@yandex.ru>
2020-06-06 11:34:17 +0100

    Some tests

--- decor.js
+++ decor.js
@@ -2,47 +2,10 @@ DecorAddButton.onclick = function(){
 
     // сворачивает и разворачивает список с картинками для декора
 
-    if (fireplaceDecor.style.display == "none") {
-        fireplaceDecor.style.display = "block";
+    if (decorations.style.display == "none") {
+      decorations.style.display = "block";
     }
     else {
-        fireplaceDecor.style.display = "none";
+      decorations.style.display = "none";
     }
 }
-
-fireplaceDecor.onmousedown = function(event) {
-
-    let shiftX = event.clientX - fireplaceDecor.getBoundingClientRect().left;
-    let shiftY = event.clientY - fireplaceDecor.getBoundingClientRect().top;
-  
-    fireplaceDecor.style.position = 'absolute';
-    fireplaceDecor.style.zIndex = 1000;
-    document.body.append(fireplaceDecor);
-  
-    moveAt(event.pageX, event.pageY);
-  
-    // переносит мяч на координаты (pageX, pageY),
-    // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-    function moveAt(pageX, pageY) {
-      fireplaceDecor.style.left = pageX - shiftX + 'px';
-      fireplaceDecor.style.top = pageY - shiftY + 'px';
-    }
-  
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-    }
-  
-    // передвигаем мяч при событии mousemove
-    document.addEventListener('mousemove', onMouseMove);
-  
-    // отпустить мяч, удалить ненужные обработчики
-    fireplaceDecor.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      fireplaceDecor.onmouseup = null;
-    };
-  
-  };
-  
-  fireplaceDecor.ondragstart = function() {
-    return false;
-  };
